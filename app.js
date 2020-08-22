e6882ccb5dd4fb47f2ff659439190f403220e7b1
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-08-22 14:33:46 +0800

    add search function

--- app.js
+++ app.js
@@ -13,15 +13,29 @@ app.set("view engine", "handlebars")
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-  res.render("index", { restaurant: restaurantList.results })
+  res.render("index", { restaurants: restaurantList.results })
+})
+
+//querystring取得問號內容
+app.get("/search", (req, res) => {
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return (
+      restaurant.name_en
+        .toLowerCase()
+        .includes(req.query.keyword.toLowerCase()) ||
+      restaurant.name.includes(req.query.keyword) ||
+      restaurant.category.includes(req.query.keyword)
+    )
+  })
+  res.render("index", { restaurants: restaurants, keyword: req.query.keyword })
+  // res.send("this is first project")
 })
 
 // params
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  console.log(req.params.restaurant_id)
+app.get("/restaurants/:restaurant_name", (req, res) => {
   const restaurant = restaurantList.results.filter(function(restaurant) {
-    return restaurant.id === Number(req.params.restaurant_id)
-    // req.params.restaurant_id 是字串 須加number 或用雙等號
+    return restaurant.name === req.params.restaurant_name
+    // req.params.item_id 會是字串 須加number 或用雙等號
   })
   res.render("show", {
     restaurant: restaurant[0]
